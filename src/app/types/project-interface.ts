d818224b64056c79072b72d42de8f22f7b5292f5
Masayuki Inukai <mzinukai@up.edu.ph>
2025-07-25 13:12:39 +0800

    feat: make interface for project and skills

---
+++ src/app/types/project-interface.ts
@@ -0,0 +1,9 @@
+export interface Project {
+    projectId: number;
+    projectTitle: string;
+    projectDescription: string;
+    technologies: string[];
+    thumbnail: string;
+    linkToLive?: string | null;
+    linkToRepo?: string | null;
+}
