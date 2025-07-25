23b9708b6cff00e6b62849520f80a7a4ce663265
Masayuki Inukai <mzinukai@up.edu.ph>
2025-07-25 12:51:55 +0800

    feat: finish supabase services for skills and projects

--- src/app/services/supabase/projects-supabase.service.ts
+++ src/app/services/supabase/projects-supabase.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { createClient } from '@supabase/supabase-js';
+import { environment } from '../../../environments/environment';
+import { Database } from '../../types/database.types';
+import { from, map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectsSupabaseService {
+  supabase = createClient<Database>(
+    environment.supabaseUrl,
+    environment.supabaseKey
+  );
 
-  constructor() { }
+  getProjects(): Observable<any> {
+    const promise = this.supabase.from('projects').select('*');
+    return from(promise).pipe(
+      map(response => {
+        if (response.error) {
+          console.error('Error fetching projects:', response.error);
+          return [];
+        }
+        return response.data;
+      })
+    );
+  }
 }
--- src/app/services/supabase/skills-supabase.service.ts
+++ src/app/services/supabase/skills-supabase.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { createClient } from '@supabase/supabase-js';
+import { Database } from '../../types/database.types';
+import { environment } from '../../../environments/environment';
+import { from, map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SkillsSupabaseService {
 
-  constructor() { }
+  supabase = createClient<Database>(
+    environment.supabaseUrl,
+    environment.supabaseKey
+  );
+
+  getSkills(): Observable<any> {
+    const promise = this.supabase.from('skills').select('*');
+    return from(promise).pipe(
+      map(response => {
+        if (response.error) {
+          console.error('Error fetching skills:', response.error);
+          return [];
+        }
+        return response.data;
+      })
+    );
+  }
 }
